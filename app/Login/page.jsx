000b0bec41632cf2249a3376bfd097d01675d054
AlexisBroh <aamedinag62@gmail.com>
2023-02-17 12:13:26 -0600

    maquetacion, falta conexion con el back

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -1,7 +1,14 @@
 export default function Home() {
     return (
         <>
-            
+        <body>
+            <h1>Login</h1>
+           <label for="email">Email</label>
+        <input type="email" id="email" name="email" />
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" /> 
+        </body>
+        
             
        </>
     
--- app/Register/page.jsx
+++ app/Register/page.jsx
@@ -9,11 +9,16 @@ export default function Home() {
         <input type="text" id="first" name="first" />
         <label for="last">Last name:</label>
         <input type="text" id="last" name="last" />
-        <label for="first">First name:</label>
-        <input type="text" id="first" name="first" />
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" />
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" />
         <label for="datebirth">Birthdate</label>
-        <input type="date" id="datebirth" name="datebirth" />
+        <input type="date" id="birthdate" name="birthdate" />
         <button type="submit">Submit</button>
+        <label htmlFor="Facebook">Facebook</label>
+        <br />
+        <label htmlFor="Google">Google</label>
         </form>
         </body>
        </>
